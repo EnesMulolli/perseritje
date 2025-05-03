91af524d8643ff2070f87ea9dabd016d13ef421e
Student <enesmulolli59@gmail.com>
2025-05-03 03:48:11 -0700

    .....

--- OOP's/main.py
+++ OOP's/main.py
@@ -9,5 +9,30 @@ p1 = Person("Enes")
 p1.greet()
 
 
+class Rectangle:
+    def __init__(self, height, length):
+        self.height = height
+        self.length = length
+    def Area(self):
+        return self.height * self.length
 
+calculate = Rectangle(5, 12)
+
+print(calculate.Area())
+
+
+
+class Pets:
+    def __init__(self, name, age, species, breed, color):
+        self.name = name
+        self.age = age
+        self.species = species
+        self.breed = breed
+        self.color = color
+    def makeSound(self):void
+
+        if result == "dog":
+            print("Hum Hum")
+        elif result == "kitten":
+            print("Meow")
 
