20c2428eb194fda59fd66aa80f55122bed3f30b0
Student <enesmulolli59@gmail.com>
2025-05-03 02:47:44 -0700

    .....

--- OOP's/main.py
+++ OOP's/main.py
@@ -1,10 +1,13 @@
-#OOP-> Nje menyre per organizim te kodit permes objects
-#Object-> Ka veti dhe sjellje
-#Class-> Eshte blueprint ose skice
-# OML-> Object Modeling Language
 
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def greet(self):
+        print(f"Hii, I'm {self.name}")
 
+p1 = Person("Enes")
 
+p1.greet()
 
 
 
